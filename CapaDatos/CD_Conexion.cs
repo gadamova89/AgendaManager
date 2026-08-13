bae7e4db093573a8c62ed14708fa0a1cb31df228
gadamova89 <gadamova89@gmail.com>
2026-08-13 13:47:08 -0400

    Feat: Arquitectura base, organizacion de proyectos por capa, carpetas.

---
+++ CapaDatos/CD_Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    internal class CD_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=; Database=; trustservercertificate=true");
+        
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+            {
+                Conexion.Open();
+            }
+
+            if (Conexion.State == ConnectionState.Open)
+            {
+                Conexion.Close();
+            }
+            return Conexion;
+        }
+    }
+
+}
